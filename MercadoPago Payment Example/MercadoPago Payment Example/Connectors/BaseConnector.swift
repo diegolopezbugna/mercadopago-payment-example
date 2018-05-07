5a4a13a71d7c47568c508432bf2a763ae5ef93ae
Diego López Bugna <diegolopezbugna@gmail.com>
2018-05-07 12:21:39 -0300

    more error handling

--- MercadoPago Payment Example/MercadoPago Payment Example/Connectors/BaseConnector.swift
+++ MercadoPago Payment Example/MercadoPago Payment Example/Connectors/BaseConnector.swift
@@ -10,22 +10,24 @@ import Foundation
 
 class BaseConnector {
 
-    let publicKey: String = "444a9ef5-8a6b-429f-abdf-587639155d88" // TODO: remove hardcoded
+    let publicKey = "444a9ef5-8a6b-429f-abdf-587639155d88" // TODO: remove hardcoded
 
     func requestDecodable<T: Decodable>(urlString: String, completion: @escaping (T?) -> ()) {
-        // TODO: throws
-        // TODO: alamoFire?
         // TODO: Modularizar la llamada para recibir base_url, uri y query_params.
 
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
-        let url = URL(string: urlString)! // TODO: remove !
-        NSLog("URL: %@", url.absoluteString)
-        
+        guard let url = URL(string: urlString) else {
+            NSLog("ERROR: can't form url from \(urlString)")
+            completion(nil)
+            return
+        }
+        NSLog("URL: \(url)")
+
         let task = session.dataTask(with: url) { (data, response, error) in
             
-            guard error == nil else {
-                NSLog("ERROR: %@", error!.localizedDescription)
+            if let error = error {
+                NSLog("ERROR: \(error)")
                 completion(nil)
                 return
             }
@@ -36,9 +38,14 @@ class BaseConnector {
             }
             
             let decoder = JSONDecoder()
-            let decodable = try! decoder.decode(T.self, from: data) // TODO: try? ERRORS!
-            print("decoded: ", decodable)
-            completion(decodable)
+            do {
+                let decodable = try decoder.decode(T.self, from: data)
+                print("decoded: ", decodable)
+                completion(decodable)
+            } catch {
+                NSLog("ERROR PARSING: \(error)")
+                completion(nil)
+            }
         }
         task.resume()
     }
